3d92ae5a498b14932f8532c581765f1597904957
Trip cubbison <119246941+TripCu@users.noreply.github.com>
2023-12-17 20:54:32 -0500

    hmmm

--- main.py
+++ main.py
@@ -37,25 +37,26 @@ def stop_typing_sequence():
 # Create the main window
 root = tk.Tk()
 root.title("Copy Paster")
+root.config(bg='black')
 
-# Create a scrolled text area
-text_area = scrolledtext.ScrolledText(root, wrap=tk.WORD, width=40, height=10)
+# Create a scrolled text area with black background and white text
+text_area = scrolledtext.ScrolledText(root, wrap=tk.WORD, width=40, height=10, bg='black', fg='white')
 text_area.pack(padx=10, pady=10)
 
-# Create a Save button
-button_save = tk.Button(root, text="Save", command=save_to_file)
+# Create a Save button (yellow with black text)
+button_save = tk.Button(root, text="Save", command=save_to_file, bg='yellow', fg='black')
 button_save.pack(pady=5)
 
-# Create a Run button
-button_run = tk.Button(root, text="Run", command=run_typing_sequence)
+# Create a Run button (green with white text)
+button_run = tk.Button(root, text="Run", command=run_typing_sequence, bg='green', fg='white')
 button_run.pack(pady=5)
 
-# Create a Stop button
+# Create a Stop button (red with white text)
 button_stop = tk.Button(root, text="Stop", command=stop_typing_sequence, bg='red', fg='white')
 button_stop.pack(pady=5)
 
 # Status label
-label_status = tk.Label(root, text="")
+label_status = tk.Label(root, text="", bg='black', fg='white')
 label_status.pack()
 
 # Run the application
